a2c5b724f9dcba32b5e3783e82e316f39c958e60
Birte Spreuer <mail@birtespreuer.com>
2020-03-04 09:42:36 +0100

    birte is awesome2

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,9 @@ const Form = props => (
     onSubmit={props.getArticles}
     style={{ marginBottom: "2rem" }}
   >
-    <span>Enter Animal here!</span>
+    <span>
+      Search Human News. <br></br>Enter Species here!
+    </span>
     <div class="arrow bounce"></div>
     <input className="form__input" type="text" name="articleName" />
     <button className="btn">Check it out!</button>
