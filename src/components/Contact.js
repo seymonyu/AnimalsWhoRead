4c2b3a909fb60a7b1adf679b53dd5ef43df5dd9c
Ligita <legyta@gmail.com>
2020-03-04 09:38:05 +0100

    about humans info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,18 @@ function Contact(props) {
     <div className="contacts-page" id="contact">
       <div className="about-paws">
         <h1> About Paws </h1>
+        <p>
+          Not all stinky humans are bad! There are humans out there who want to
+          help animals to stand for their rights! That is why we have worked
+          hard to provide this one and only website for Animals Who Read. By
+          doing so - we believe that we empower animals to have access to the
+          news. Which in turn means - empowering animals to act! We aim to
+          provide a platform for Animals where they can feel connected, express
+          their feelings and thoughts and most important - learn about what
+          other animals are up to. If you would like to contact us, or if you
+          would like to see more things in this website - please reach out! We
+          are open and verry happy to help!
+        </p>
         <img src={Paw} className="navbar__brand-img" alt="logo" />
       </div>
       <Footer />
